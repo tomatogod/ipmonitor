33c35ce4e52cdb3c015b4c18501e19b616a325cd
tomatogod <tomatogod@gmail.com>
2020-10-24 14:48:07 +0100

    Change to use list of hosts and move socket check to a function

--- IPMonitor.py
+++ IPMonitor.py
@@ -6,20 +6,26 @@ import datetime
 
 print("---IP Port Monitor Started---\n")
 
-host = '192.168.1.1'
+hosts = ['192.168.1.1', '192.168.1.2', '192.168.1.188', '192.168.1.253', '192.168.1.254']
 port = 80
 
-while True:
+def time_now():
+    time_is = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
+    return time_is
 
-    time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
+def check_up():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
+    s.settimeout(0.1)
     result = s.connect_ex((host, port))
     s.close()
+    return result
 
-    if result:
-        print(time_now + str(host) + ":" + str(port) + " looks down")
-    else:
-        print(time_now + str(host) + ":" + str(port) + " looks up")
-
+while True:
+    for host in hosts:
+        if check_up():
+            print(str(time_now()) + str(host) + ":" + str(port) + " looks down")
+        else:
+            #pass
+            print(str(time_now()) + str(host) + ":" + str(port) + " looks up")
+    print("\n")
     time.sleep(1)
